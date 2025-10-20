552f8ef46e58c1d9b191a8b55f203eca28eb86d0
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-20 11:52:21 +0900

    Membuat method GET

--- index.js
+++ index.js
@@ -27,3 +27,14 @@ db.connect((err) => {
     }
     console.log('Connected to the MySQL database.');
 });
+
+app.get('/api/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err.stack);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    });
+});
